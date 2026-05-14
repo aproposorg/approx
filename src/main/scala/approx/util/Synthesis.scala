45d19ca2aa23420deebee0342a790f5f7909ac27
Hans Jakob Damsgaard <hansjakobdamsgaard@gmail.com>
2026-05-14 22:59:58 +0200

    enable generating and opening a Vivado project

--- src/main/scala/approx/util/Synthesis.scala
+++ src/main/scala/approx/util/Synthesis.scala
@@ -53,20 +53,26 @@ object Synthesis {
 
       // Generate helper Makefile
       val make = s"""
-        |SV_FILE            =${svFile}
+        |SV_FILE            :=${svFile}
         |
-        |VIVADO_SYN_TCL     =${topName}_syn.tcl
-        |VIVADO_SYN_DCP     =${topName}_syn.dcp
-        |VIVADO_SYN_REPORT  =${topName}_syn.rpt
+        |VIVADO_PART        :=${part}
         |
-        |VIVADO_IMPL_TCL    =${topName}_impl.tcl
-        |VIVADO_IMPL_DCP    =${topName}_impl.dcp
-        |VIVADO_IMPL_REPORT =${topName}_impl.rpt
+        |VIVADO_PROJ_DIR    :=${topName}
+        |VIVADO_PROJ_TCL    :=${topName}_proj.tcl
+        |VIVADO_PROJ_XPR    :=$$(VIVADO_PROJ_DIR)/${topName}.xpr
+        |
+        |VIVADO_SYN_TCL     :=${topName}_syn.tcl
+        |VIVADO_SYN_DCP     :=${topName}_syn.dcp
+        |VIVADO_SYN_REPORT  :=${topName}_syn.rpt
+        |
+        |VIVADO_IMPL_TCL    :=${topName}_impl.tcl
+        |VIVADO_IMPL_DCP    :=${topName}_impl.dcp
+        |VIVADO_IMPL_REPORT :=${topName}_impl.rpt
         |
         |# Macros to generate TCL scripts for synthesis and implementation
         |define VIVADO_SYN_TCL_CONTENT
-        |read_verilog ${svFile}
-        |synth_design -top ${topName} -part ${part}
+        |read_verilog $$(SV_FILE)
+        |synth_design -top ${topName} -part $$(VIVADO_PART)
         |report_utilization -file $$(VIVADO_SYN_REPORT)
         |write_checkpoint -force $$(VIVADO_SYN_DCP)
         |
@@ -89,11 +95,49 @@ object Synthesis {
         |$$(VIVADO_IMPL_TCL):
         |\t$$(file >$$@,$$(VIVADO_IMPL_TCL_CONTENT))
         |
+        |define VIVADO_PROJ_TCL_CONTENT
+        |create_project -force $$(VIVADO_PROJ_XPR) -part $$(VIVADO_PART)
+        |add_files $$(SV_FILE)
+        |set_property top ${topName} [current_fileset]
+        |update_compile_order -fileset sources_1
+        |
+        |endef
+        |
+        |$$(VIVADO_PROJ_TCL):
+        |\t$$(file >$$@,$$(VIVADO_PROJ_TCL_CONTENT))
+        |
+        |# Helpers to generate and open a Vivado project for interactive exploration
+        |$$(VIVADO_PROJ_XPR): $$(VIVADO_PROJ_TCL)
+        |\tvivado -nolog -nojournal -mode batch -source $$<
+        |
+        |.PHONY: generate-vivado-project
+        |# Generate Vivado project file
+        |generate-vivado-project: $$(VIVADO_PROJ_XPR)
+        |
+        |.PHONY: open-vivado-gui
+        |# Open Vivado GUI with the generated project
+        |open-vivado-gui: $$(VIVADO_PROJ_XPR)
+        |\tvivado -nolog -nojournal $$<
+        |
+        |.PHONY: open-vivado-tcl
+        |# Open Vivado TCL with the generated project
+        |open-vivado-tcl: $$(VIVADO_PROJ_XPR)
+        |\tvivado -nolog -nojournal -mode tcl $$<
+        |
         |# Helper clean targets for generated files
+        |.PHONY: clean-vivado
+        |# Remove all generated Vivado files
+        |clean-vivado: clean-vivado-project clean-vivado-tcl clean-vivado-rpt clean-vivado-dcp
+        |
+        |.PHONY: clean-vivado-project
+        |# Remove generated Vivado project files
+        |clean-vivado-project:
+        |\trm -rf $$(VIVADO_PROJ_DIR)
+        |
         |.PHONY: clean-vivado-tcl
         |# Remove generated TCL scripts
         |clean-vivado-tcl:
-        |\trm -f $$(VIVADO_SYN_TCL) $$(VIVADO_IMPL_TCL)
+        |\trm -f $$(VIVADO_PROJ_TCL) $$(VIVADO_SYN_TCL) $$(VIVADO_IMPL_TCL)
         |
         |.PHONY: clean-vivado-rpt
         |# Remove generated reports
@@ -106,14 +150,14 @@ object Synthesis {
         |\trm -f $$(VIVADO_SYN_DCP) $$(VIVADO_IMPL_DCP)
         |
         |$$(VIVADO_SYN_REPORT): $$(SV_FILE) $$(VIVADO_SYN_TCL)
-        |\tvivado -mode batch -source $$(VIVADO_SYN_TCL)
+        |\tvivado -nolog -nojournal -mode batch -source $$(VIVADO_SYN_TCL)
         |
         |.PHONY: vivado-syn
         |# Run synthesis to generate synthesis report $$(VIVADO_SYN_REPORT)
         |vivado-syn: $$(VIVADO_SYN_REPORT)
         |
         |$$(VIVADO_IMPL_REPORT): $$(VIVADO_SYN_REPORT) $$(VIVADO_IMPL_TCL)
-        |\tvivado -mode batch -source $$(VIVADO_IMPL_TCL)
+        |\tvivado -nolog -nojournal -mode batch -source $$(VIVADO_IMPL_TCL)
         |
         |.PHONY: vivado-impl
         |# Run implementation to generate implementation report $$(VIVADO_IMPL_REPORT)
